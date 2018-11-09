fb5c28e7c6b3f0c5deb9e4d7b40908dbea2146c6
jiawei2333 <yujiawei233@gmail.com>
2018-11-10 02:07:40 +0800

    Modified confusion matrix & Add SVM

--- Digit_recognizer.py
+++ Digit_recognizer.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_val_score
 
 df_train = pd.read_csv(os.path.join(os.getcwd(), 'kaggle/Digit_recognizer/train.csv'))
 df_test = pd.read_csv(os.path.join(os.getcwd(), 'kaggle/Digit_recognizer/test.csv'))
@@ -28,17 +27,28 @@ for i in range(10):
     fig.add_subplot(2, 5, i+1)
     X1 = X_test[i].reshape(28,28)
     plt.imshow(X1)
+	plt.axis('off')
 
 # Plot confusion matrix
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 
-X_train_R, X_test_R, y_train_R, y_test_R = train_test_split(X_train, y_train)
-predictions = nn.predict(X_test_R)
-print(classification_report(y_test_R, predictions))
-confusion = confusion_matrix(y_test_R, predictions)
+y_predict = cross_val_predict(nn, X_train, y_train, cv = 3)
+print(classification_report(y_train, y_predict))
+confusion = confusion_matrix(y_train, y_predict)
 plt.matshow(confusion)
 plt.colorbar()
 plt.ylabel('True label')
 plt.xlabel('Predicted label')
 plt.show()
+
+# SVM
+from sklearn.svm import SVC
+
+SVM_clf = SVC(kernel = 'linear', gamma = 0.1, C = 10, random_state = 0)
+SVM_clf.fit(X_train, y_train)
+
+predictions_svm = SVM_clf.predict(X_test)
+print(predictions_svm[:10])
+
+scores_svm = cross_val_score(SVM_clf, X_train, y_train, scoring='accuracy', cv=2)
+print('Acurracy for rbf SVM is: ', scores_svm)
